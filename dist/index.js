cd146be02d594cd801b9638dd124e90524fb94bf
Isaac Besora Vilardaga <isaac@altersport.cat>
2019-01-11 13:00:40 +0100

    Exports AcceptDialog, Popup, ProgressDialog and QuestionDialog

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,8 @@
-import Map from "./components/Map";
+import AcceptDialog from "./components/AcceptDialog";
 import Bubble from "./components/Bubble";
+import Map from "./components/Map";
+import Popup from "./components/Popup";
+import ProgressDialog from "./components/ProgressDialog";
+import QuestionDialog from "./components/QuestionDialog";
 
-export { Map, Bubble };
+export { AcceptDialog, Bubble, Map, Popup, ProgressDialog, QuestionDialog };
--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 //@flow
 
-import Map from "./components/Map";
+import AcceptDialog from "./components/AcceptDialog";
 import Bubble from "./components/Bubble";
+import Map from "./components/Map";
+import Popup from "./components/Popup";
+import ProgressDialog from "./components/ProgressDialog";
+import QuestionDialog from "./components/QuestionDialog";
 
-export { Map, Bubble };
+export { AcceptDialog, Bubble, Map, Popup, ProgressDialog, QuestionDialog};
